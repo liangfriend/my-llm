839b679f08c6d4475b68c97ff504273036ed8180
liangyouyi <liangyouyi@jinsanhui.com>
2025-12-19 10:13:12 +0800

    增加日志

--- melody-model/lib/storage.ts
+++ melody-model/lib/storage.ts
@@ -2,6 +2,7 @@
 import { DATA_FILE } from './constants';
 import { RawNote } from './note';
 import {TrainingData} from "../type";
+import logger from "./logger";
 
 
 // 保存训练数据
@@ -9,11 +10,16 @@ export function loadTrainingData(): TrainingData {
   try {
     const content = fs.readFileSync(DATA_FILE, 'utf8');
     const data: unknown = JSON.parse(content);
+      console.log('样本数据',data)
+      logger.log('样本数据',data)
     if (!data || !Array.isArray((data as any).examples)) {
+
       return { examples: [] };
     }
     return data as TrainingData;
   } catch (err) {
+      console.log('样本数据获取不到',err)
+      logger.error('样本数据获取不到',err)
     return { examples: [] };
   }
 }
